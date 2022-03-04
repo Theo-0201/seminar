30932c4519c157201e45f7cbc7fd343488502df7
Theodora Gita <theodoragita@yahoo.com>
2022-03-04 17:06:05 +0200

    commit1

---
+++ eclipse-workspace/Seminar2/Seminar2/src/ro/ase/cts/g1098/recap/exceptions/InsufficientFundsException.java
@@ -0,0 +1,13 @@
+package ro.ase.cts.g1098.recap.exceptions;
+
+public class InsufficientFundsException extends Exception{
+
+	public InsufficientFundsException(String msg) {
+		super(msg);
+	}
+	
+	public InsufficientFundsException() {
+		//super();
+	}
+	
+}
